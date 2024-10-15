0b949a9cc88d8fc5b9f4b672de54a074e2dedeee
TylerSocholotuik <143831033+TylerSocholotuik@users.noreply.github.com>
2024-10-15 09:15:52 -0600

    updated theme

--- training-log-app/utils/theme/theme.js
+++ training-log-app/utils/theme/theme.js
@@ -1,12 +1,21 @@
 import { createTheme } from '@mui/material/styles';
 import { deepPurple, indigo } from '@mui/material/colors';
 
-const theme = createTheme({  
-    palette: {
-        mode: 'dark',
-        primary: deepPurple,
-        secondary: indigo
-    },
+const theme = createTheme({ 
+    colorSchemes: {
+        light: {
+            palette: {
+                primary: deepPurple,
+                secondary: indigo
+            },
+        },
+        dark: {
+            palette: {
+                primary: deepPurple,
+                secondary: indigo
+            },
+        },
+    }, 
     typography: {
         fontSize: 8
     }
