2050ac8cccf4eb4ee5dc858575c66c6f40411552
TylerSocholotuik <143831033+TylerSocholotuik@users.noreply.github.com>
2024-10-20 09:47:52 -0600

    tested exercise.json file using MUI autocomplete component

--- training-log-app/pages/exercises.js
+++ training-log-app/pages/exercises.js
@@ -1,9 +1,43 @@
+import { useEffect, useState } from 'react';
+
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+
 export default function Exercises() {
-    return (
-        <>
-          <main>
-            
-          </main>
-        </>
-      );
+
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    // Fetch the data from the public folder
+    fetch('/data/exercises.json')
+      .then(response => response.json())
+      .then(data => setData(data))
+      .catch(error => console.error('Error fetching the data:', error));
+  }, []);
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
+  const options = data.exercises.map((exercise) => {
+    const firstLetter = exercise.name[0].toUpperCase();
+    return {
+      firstLetter: /[0-9]/.test(firstLetter) ? '0-9' : firstLetter,
+      ...exercise,
+    };
+  });
+
+  return (
+    <>
+      <main>
+        <Autocomplete
+          options={options.sort((a, b) => -b.firstLetter.localeCompare(a.firstLetter))}
+          groupBy={(exercise) => exercise.firstLetter}
+          getOptionLabel={(exercise) => exercise.name}
+          sx={{ width: 300 }}
+          renderInput={(params) => <TextField {...params} label="Select Exercise" />}
+        />
+      </main>
+    </>
+  );
 }
